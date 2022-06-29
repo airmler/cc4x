3511b9f9c217124edfdd2d6b0f6daed1c4d5eb32
Andreas Irmler <andreas.irmler@tuwien.ac.at>
2022-06-29 17:22:58 +0200

    add the mk file mentioned

---
+++ etc/cc4x.mk
@@ -0,0 +1,24 @@
+# Provide 
+
+
+CXX=mpic++
+CXXFLAGS := -std=c++11 -O3
+
+
+
+CTF_PATH = /home/irmler/Programs/cc4x/ctf
+YAML_PATH = /home/irmler/Programs/cc4s_release/extern/build/gcc-oblas-ompi/yaml-cpp/c9460110e072df84b7dee3eb651f2ec5df75fb18
+
+
+# BACKENDS
+CXXFLAGS += -DCTFBS_BACKEND
+
+
+CXXFLAGS += -I${CTF_PATH}/include
+CXXFLAGS += -I.
+CXXFLAGS += -I${YAML_PATH}/include
+
+LIBS += -L${CTF_PATH}/lib -lctf
+LIBS += -L/opt/OpenBLAS/lib/ -lopenblas -fopenmp -lscalapack -lgfortran
+LIBS += -lyaml-cpp -L${YAML_PATH}/lib
+
